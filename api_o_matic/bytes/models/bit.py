c058436dba53f1b8e59eccb266f750d829076c5b
Leohakim <leohakim@gmail.com>
2021-09-18 17:37:47 +0200

    Add Bytes App models and migration

---
+++ api_o_matic/bytes/models/bit.py
@@ -0,0 +1,36 @@
+import uuid
+
+from django.db.models import PROTECT, CharField, ForeignKey, UUIDField
+from django.utils.translation import gettext_lazy as _
+
+from api_o_matic.bytes.models.base_model import BaseModel
+from api_o_matic.bytes.models.source import Source
+
+
+class Bit(BaseModel):
+    """Bit model for Api-o-Matic"""
+
+    TYPE_STRING = "string"
+    TYPE_NUMBER = "number"
+    TYPE_DATE = "date"
+    TYPE_DATETIME = "datetime"
+
+    TYPE_CHOICES = (
+        (TYPE_STRING, "String"),
+        (TYPE_NUMBER, "Number"),
+        (TYPE_DATE, "Date"),
+        (TYPE_DATETIME, "DateTime"),
+    )
+
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    name = CharField(_("Name of Bit"), blank=False, max_length=255)
+    description = CharField(_("Description of Bit"), blank=True, max_length=255)
+    type_bit = CharField(
+        _("Type of Bit"), choices=TYPE_CHOICES, max_length=50, blank=False
+    )
+    path = CharField(_("Path of Bit"), max_length=250, blank=False)
+    value = CharField(_("Value of Bit"), max_length=250, blank=True)
+    source = ForeignKey(Source, on_delete=PROTECT)
+
+    def __str__(self):
+        return self.name
---
+++ api_o_matic/bytes/models/source.py
@@ -0,0 +1,28 @@
+import uuid
+
+from django.db.models import CharField, URLField, UUIDField
+from django.utils.translation import gettext_lazy as _
+
+from api_o_matic.bytes.models.base_model import BaseModel
+
+
+class Source(BaseModel):
+    """Source model for Api-o-Matic"""
+
+    TYPE_HTML = "html"
+    TYPE_JSON = "json"
+
+    TYPE_CHOICES = (
+        (TYPE_HTML, "HTML"),
+        (TYPE_JSON, "JSON"),
+    )
+
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    url = URLField(_("URL of Source"), blank=False)
+    description = CharField(_("Description of Bit"), blank=True, max_length=255)
+    type_source = CharField(
+        _("Type of Source"), choices=TYPE_CHOICES, max_length=50, blank=False
+    )
+
+    def __str__(self):
+        return self.url
