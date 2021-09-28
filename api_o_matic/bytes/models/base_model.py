8a99e08b4e104be4f64681a858c01e0e8dd102ea
Leohakim <leohakim@gmail.com>
2021-09-28 12:55:06 +0200

    Add editable=False on models, translate Basemodel

--- api_o_matic/bytes/models/base_model.py
+++ api_o_matic/bytes/models/base_model.py
@@ -1,15 +1,16 @@
 from django.conf import settings
 from django.db.models import PROTECT, BooleanField, DateTimeField, ForeignKey, Model
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(Model):
-    is_active = BooleanField("is active?", default=True)
+    is_active = BooleanField(_("Is active?"), default=True)
     created_by = ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=PROTECT,
     )
-    created_at = DateTimeField("created on", auto_now_add=True)
-    updated_at = DateTimeField("last modified on", auto_now=True)
+    created_at = DateTimeField(_("Created on"), auto_now_add=True)
+    updated_at = DateTimeField(_("Last modified on"), auto_now=True)
 
     def delete(self, using=None, keep_parents=False):
         self.is_active = False
--- api_o_matic/bytes/models/byte.py
+++ api_o_matic/bytes/models/byte.py
@@ -10,7 +10,7 @@ from api_o_matic.bytes.models.bit import Bit
 class Byte(BaseModel):
     """Byte model for Api-o-Matic"""
 
-    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(_("Name of Byte"), blank=False, max_length=255)
     description = CharField(_("Description of Byte"), blank=True, max_length=255)
     bits = ManyToManyField(Bit, blank=True)
